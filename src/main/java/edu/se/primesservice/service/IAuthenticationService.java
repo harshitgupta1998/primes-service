ae1251de134d869ab476148f985aa5a734627538
Harshit Gupta <31321261+harshitgupta1998@users.noreply.github.com>
2024-04-04 17:12:09 -0400

    Update IAuthenticationService.java

--- src/main/java/edu/se/primesservice/service/IAuthenticationService.java
+++ src/main/java/edu/se/primesservice/service/IAuthenticationService.java
@@ -5,7 +5,6 @@ import edu.se.primesservice.model.Customer;
 import java.io.IOException;
 
 public interface IAuthenticationService {
-
-    boolean register(Customer customer) throws IOException;
-    boolean login(String username,String password) throws IOException;
+    Customer register(Customer customer) throws IOException;
+    boolean login(String username, String password) throws IOException;
 }
